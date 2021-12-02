7a7f3edea9b6d3b42bdf1024f5645c8cc1a475c9
zhiyeonyi <yjy39k@naver.com>
2021-12-02 20:59:25 +0900

    1강 완료

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 const Text = (props) => {
 
-    const {bold, color, size, children} = props;
+    const {bold, color, size, children, margin} = props;
 
-    const styles = {bold:bold, color:color, size:size};
+    const styles = {bold:bold, color:color, size:size, margin};
     return (
         <P {...styles}>
             {children}
@@ -19,7 +19,7 @@ Text.defaultProps = {
     bold: false,
     color: '#222831',
     size: '14px',
-
+    margin: false,
 
 }
 
@@ -27,6 +27,7 @@ const P = styled.p`
     color: ${(props) => props.color};
     font-size: ${(props)=> props.size};
     font-weight: ${(props)=> (props.bold? "600": "400")};
+    ${(props) => (props.margin? `margin: ${props.margin};` : '')}
 ` 
 
 export default Text;
